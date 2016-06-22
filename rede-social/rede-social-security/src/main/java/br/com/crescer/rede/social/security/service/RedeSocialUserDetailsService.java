5a04bf9c54b4e60c5180a32fd73ba2f7d7e147f0
Vinicius Ambrosi <ambrosi.vinicius@gmail.com>
2016-06-22 16:42:33 -0300

    feat(userdetails): adicionar novos atributos ao usermodel, e retonar user correto.

--- rede-social/rede-social-security/src/main/java/br/com/crescer/rede/social/security/service/RedeSocialUserDetailsService.java
+++ rede-social/rede-social-security/src/main/java/br/com/crescer/rede/social/security/service/RedeSocialUserDetailsService.java
@@ -1,11 +1,11 @@
 package br.com.crescer.rede.social.security.service;
 
 import br.com.crescer.entity.Perfil;
+import br.com.crescer.entity.Pessoa;
 import br.com.crescer.rede.social.security.model.UserModel;
-import br.com.crescer.service.UsuarioService;
+import br.com.crescer.service.PerfilService;
 import br.com.crescer.social.security.enumeration.SocialRoles;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -17,15 +17,17 @@ import org.springframework.stereotype.Service;
 public class RedeSocialUserDetailsService implements UserDetailsService {
 
     @Autowired
-    UsuarioService usuarioServico;
+    PerfilService usuarioServico;
 
     @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+    public UserModel loadUserByUsername(String email) throws UsernameNotFoundException {
         if (email.isEmpty()) {
             throw new UsernameNotFoundException(String.format("User with email=%s was not found", email));
         }
         Perfil perfil = usuarioServico.findOneByEmail(email);
+        Pessoa pessoa = perfil.getPessoaIdPessoa();
         return new UserModel(perfil.getDsEmail(), perfil.getDsSenha(), 
-                SocialRoles.valuesToList(), perfil.getPessoaIdPessoa().getNmPessoa());
+                SocialRoles.valuesToList(), pessoa.getNmPessoa(), pessoa.getIdPessoa(), 
+                perfil.getDsEmail(), pessoa.getDtNascPessoa(), pessoa.getTpSexoPessoa());
     }
 }
