ef80d9719a72984a63a83c5135f8c66c0770ae73
Vinicius Ambrosi <ambrosi.vinicius@gmail.com>
2016-06-22 20:21:45 -0300

    feat(perfil): adicionar perfil de pessoas junto com botao de adicionar.

--- rede-social/rede-social-web/src/main/java/br/com/crescer/controllers/HomeController.java
+++ rede-social/rede-social-web/src/main/java/br/com/crescer/controllers/HomeController.java
@@ -1,6 +1,10 @@
 package br.com.crescer.controllers;
 
-import javax.servlet.http.HttpSession;
+import br.com.crescer.rede.social.security.model.UserModel;
+import java.security.Principal;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,8 +16,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping(value = "/home")
 public class HomeController {
+
+    @Autowired
+    PessoaController pessoaController;
+
     @RequestMapping(method = RequestMethod.GET)
     public String home(Model m) {
+        UserModel usuarioLogado
+                = (UserModel) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        pessoaController.tabelaPessoa(usuarioLogado.getId(), m);
         return "home";
     }
 }
--- rede-social/rede-social-web/src/main/java/br/com/crescer/controllers/PessoaController.java
+++ rede-social/rede-social-web/src/main/java/br/com/crescer/controllers/PessoaController.java
@@ -1,9 +1,13 @@
 package br.com.crescer.controllers;
 
+import br.com.crescer.entity.Perfil;
 import br.com.crescer.entity.Pessoa;
+import br.com.crescer.rede.social.security.model.UserModel;
+import br.com.crescer.service.PerfilService;
 import br.com.crescer.service.PessoaService;
 import java.math.BigDecimal;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,10 +19,34 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class PessoaController {
 
     @Autowired
-    PessoaService service;
+    PessoaService servicePessoa;
+
+    @Autowired
+    PerfilService servicePerfil;
 
     @RequestMapping(value = "/home/menu-lateral")
-    public String perfil(Model m) {
+    public String home(Model m) {
         return "lateral-pessoa";
+    }
+
+    @RequestMapping(value = "/perfil")
+    public String perfil(BigDecimal idPessoa, BigDecimal idPerfil, Model model) {
+        Pessoa p = servicePessoa.getPessoa(idPessoa);
+        tabelaPessoa(idPerfil, model);
+        model.addAttribute("pessoa", p);
+        return "perfil";
+    }
+
+    public String tabelaPessoa(BigDecimal bd, Model model) {
+        UserModel usuarioLogado
+                = (UserModel) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Perfil perfil = servicePerfil.getPerfil(bd);
+        boolean outroPerfil = bd.equals(usuarioLogado.getId());
+        model.addAttribute("email", perfil.getDsEmail());
+        model.addAttribute("nome", perfil.getPessoaIdPessoa().getNmPessoa());
+        model.addAttribute("idPerfil", perfil.getIdPerfil());
+        model.addAttribute("idPessoa", perfil.getPessoaIdPessoa().getIdPessoa());
+        model.addAttribute("outroPerfil", outroPerfil);
+        return "home-tabela-pessoa";
     }
 }
