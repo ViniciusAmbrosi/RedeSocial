e3802406d724e1f2a073816e0f94b4c4830a835f
Vinicius Ambrosi <ambrosi.vinicius@gmail.com>
2016-06-23 17:42:49 -0300

    feat(HeaderComponent): adicionar component para header.

---
+++ rede-social/rede-social-web/src/main/java/br/com/crescer/controllers/HeaderComponent.java
@@ -0,0 +1,39 @@
+package br.com.crescer.controllers;
+
+import br.com.crescer.entity.Perfil;
+import br.com.crescer.entity.Solicitacao;
+import br.com.crescer.service.PerfilService;
+import br.com.crescer.service.SolicitacaoService;
+import java.math.BigDecimal;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.ui.Model;
+
+/**
+ * @author vinicius.ambrosi
+ */
+@Component
+public class HeaderComponent {
+
+    private static final String SOLICITACOES = "solicitacoes";
+    private static final String SOLICITACOES_AMIZADE = "solicitacoesAmizade";
+
+    @Autowired
+    private PerfilService perfilService;
+
+    @Autowired
+    private SolicitacaoService solicitacaoService;
+
+    public void createHeader(Model model, BigDecimal idPefil) {
+        final Perfil perfil;
+        perfil = perfilService.getPerfil(idPefil);
+        
+        final List<Solicitacao> list;
+        list = solicitacaoService.getSolicitacoes(perfil);
+
+        model.addAttribute(SOLICITACOES, list);
+        model.addAttribute(SOLICITACOES_AMIZADE, list.size());
+    }
+
+}
