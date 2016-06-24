811d5a8c6f3ffaf0e84ba28973b09e0e6cfbd883
Vinicius A <Vinicius A>
2016-06-24 11:20:15 -0300

    refactor(Publicacoes): corrigir publicacoes de forma a retornar lista de forma ascendente.

--- rede-social/rede-social-service/src/main/java/br/com/crescer/repository/PublicacaoRepository.java
+++ rede-social/rede-social-service/src/main/java/br/com/crescer/repository/PublicacaoRepository.java
@@ -14,9 +14,9 @@ import org.springframework.data.repository.query.Param;
  */
 public interface PublicacaoRepository extends PagingAndSortingRepository<Publicacao, BigDecimal> {
 
-    @Query("SELECT pc FROM PublicacaoConteudo pc JOIN pc.idPublicacao pu WHERE pu.idPerfil = :perfil ORDER BY pu.dtPublicacao")
+    @Query("SELECT pc FROM PublicacaoConteudo pc JOIN pc.idPublicacao pu WHERE pu.idPerfil = :perfil ORDER BY pu.dtPublicacao DESC")
     PublicacaoConteudo findPublicacoesUsuario(@Param("perfil") Perfil perfis);
 
-    @Query("SELECT pc FROM PublicacaoConteudo pc JOIN pc.idPublicacao pu WHERE pu.idPerfil IN :perfis ORDER BY pu.dtPublicacao")
+    @Query("SELECT pc FROM PublicacaoConteudo pc JOIN pc.idPublicacao pu WHERE pu.idPerfil IN :perfis ORDER BY pu.dtPublicacao DESC")
     List<PublicacaoConteudo> findPublicacoesFriends(@Param("perfis") List<Perfil> perfis);
 }
