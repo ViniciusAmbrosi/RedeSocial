89f8e477563ef751c047c80151929076f9c2a8db
Vinicius A <Vinicius A>
2016-06-24 02:28:44 -0300

    feat(Botoes-Busca): adicionar registro de botoes de busca.

--- rede-social/rede-social-web/src/main/resources/static/js/HeaderView.js
+++ rede-social/rede-social-web/src/main/resources/static/js/HeaderView.js
@@ -4,21 +4,39 @@ function HeaderView(options) {
     options = options || {};
     this.botaoAceitar = options.botaoAceitar;
     this.botaoRejeitar = options.botaoRejeitar;
-    this.relacionamentos = new Relacionamentos({});
+    this.formBuscaPorNome = options.formBuscaPorNome;
+    this.inputBuscaPorNome = options.inputBuscaPorNome;
+    this.botaoBuscaPessoas = options.botaoBuscaPessoas;
+    this.relacionamentos = new Relacionamentos();
+    this.pessoas = new Pessoas();
 }
 
 HeaderView.prototype.registrar = function () {
     $(this.botaoAceitar.selector).click(function () {
-        this.relacionamentos.Aceitar(this.botaoAceitar.attr('data-id')).done(function(res){
+        this.relacionamentos.Aceitar(this.botaoAceitar.attr('data-id')).done(function (res) {
             $('#header').empty();
             $('#header').append(res);
         });
     }.bind(this));
 
     $(this.botaoRejeitar.selector).click(function () {
-        this.relacionamentos.Rejeitar(this.botaoRejeitar.attr('data-id')).done(function(res){
+        this.relacionamentos.Rejeitar(this.botaoRejeitar.attr('data-id')).done(function (res) {
             $('#header').empty();
             $('#header').append(res);
         });
+    }.bind(this));
+
+    $(this.formBuscaPorNome).submit(function () {
+        this.pessoas.getPessoasPorNome(this.inputBuscaPorNome.val()).done(function (res) {
+            $('#home-conteudo').empty();
+            $('#home-conteudo').append(res);
+        });
+    }.bind(this));
+
+    $(this.botaoBuscaPessoas).click(function(){
+    this.pessoas.getPessoas().done(function (res) {
+        $('#home-conteudo').empty();
+        $('#home-conteudo').append(res);
+    });
     }.bind(this));
 };
