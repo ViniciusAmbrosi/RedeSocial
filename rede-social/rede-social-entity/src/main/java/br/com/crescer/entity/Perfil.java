8109dd0a563748e6c1ac6ee53c1b5d67c8ac9293
ViniciusAmbrosi <ambrosi.vinicius@gmail.com>
2016-10-14 13:57:40 -0300

    refactor(Perfil): melhorar nome e tipos dos atributos.

--- rede-social/rede-social-entity/src/main/java/br/com/crescer/entity/Perfil.java
+++ rede-social/rede-social-entity/src/main/java/br/com/crescer/entity/Perfil.java
@@ -1,12 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package br.com.crescer.entity;
 
 import java.io.Serializable;
-import java.math.BigDecimal;
 import java.util.List;
 import javax.persistence.Basic;
 import javax.persistence.CascadeType;
@@ -17,8 +11,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.OneToMany;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
@@ -32,156 +24,118 @@ import javax.xml.bind.annotation.XmlTransient;
 @Entity
 @Table(name = "PERFIL")
 @XmlRootElement
-@NamedQueries({
-    @NamedQuery(name = "Perfil.findAll", query = "SELECT p FROM Perfil p"),
-    @NamedQuery(name = "Perfil.findByIdPerfil", query = "SELECT p FROM Perfil p WHERE p.idPerfil = :idPerfil"),
-    @NamedQuery(name = "Perfil.findByDsEmail", query = "SELECT p FROM Perfil p WHERE p.dsEmail = :dsEmail"),
-    @NamedQuery(name = "Perfil.findByDsSenha", query = "SELECT p FROM Perfil p WHERE p.dsSenha = :dsSenha")})
 public class Perfil implements Serializable {
 
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idPerfil")
-    private List<Publicacao> publicacaoList;
-
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idPerfil")
-    private List<Relacionamento> relacionamentoList;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idPerfilRelacionamento")
-    private List<Relacionamento> relacionamentoList1;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idPerfil")
-    private List<Solicitacao> solicitacaoList;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "idPerfilSolicitacao")
-    private List<Solicitacao> solicitacaoList1;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "perfil")
+    private List<Publicacao> publicacoes;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "perfil")
+    private List<Relacionamento> relacionamentos;
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "perfil")
+    private List<Solicitacao> solicitacoes;
 
     private static final long serialVersionUID = 1L;
-    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
+    
     @Id
     @Basic(optional = false)
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SEQ_PERFIL")
     @SequenceGenerator(name = "SEQ_PERFIL", sequenceName = "SEQ_PERFIL", allocationSize = 1)
     @Column(name = "ID_PERFIL")
-    private BigDecimal idPerfil;
+    private Long id;
+    
     @Basic(optional = false)
     @Column(name = "DS_EMAIL")
-    private String dsEmail;
+    private String email;
+    
     @Basic(optional = false)
     @Column(name = "DS_SENHA")
-    private String dsSenha;
+    private String senha;
+    
     @JoinColumn(name = "PESSOA_ID_PESSOA", referencedColumnName = "ID_PESSOA")
     @ManyToOne(optional = false, cascade = CascadeType.ALL)
-    private Pessoa pessoaIdPessoa;
+    private Pessoa pessoa;
 
     public Perfil() {
     }
 
-    public Perfil(BigDecimal idPerfil) {
-        this.idPerfil = idPerfil;
+    public Perfil(Long idPerfil) {
+        this.id = idPerfil;
     }
 
-    public Perfil(BigDecimal idPerfil, String dsEmail, String dsSenha) {
-        this.idPerfil = idPerfil;
-        this.dsEmail = dsEmail;
-        this.dsSenha = dsSenha;
+    public Perfil(Long id, String email, String senha) {
+        this.id = id;
+        this.email = email;
+        this.senha = senha;
     }
 
-    public BigDecimal getIdPerfil() {
-        return idPerfil;
+    public Long getId() {
+        return id;
     }
 
-    public void setIdPerfil(BigDecimal idPerfil) {
-        this.idPerfil = idPerfil;
+    public void setId(Long id) {
+        this.id = id;
     }
 
-    public String getDsEmail() {
-        return dsEmail;
+    public String getEmail() {
+        return email;
     }
 
-    public void setDsEmail(String dsEmail) {
-        this.dsEmail = dsEmail;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
-    public String getDsSenha() {
-        return dsSenha;
+    public String getSenha() {
+        return senha;
     }
 
-    public void setDsSenha(String dsSenha) {
-        this.dsSenha = dsSenha;
+    public void setSenha(String senha) {
+        this.senha = senha;
     }
 
-    public Pessoa getPessoaIdPessoa() {
-        return pessoaIdPessoa;
+    public Pessoa getPessoa() {
+        return pessoa;
     }
 
-    public void setPessoaIdPessoa(Pessoa pessoaIdPessoa) {
-        this.pessoaIdPessoa = pessoaIdPessoa;
-    }
-
-    @Override
-    public int hashCode() {
-        int hash = 0;
-        hash += (idPerfil != null ? idPerfil.hashCode() : 0);
-        return hash;
+    public void setPessoa(Pessoa pessoa) {
+        this.pessoa = pessoa;
     }
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
         if (!(object instanceof Perfil)) {
             return false;
         }
         Perfil other = (Perfil) object;
-        if ((this.idPerfil == null && other.idPerfil != null) || (this.idPerfil != null && !this.idPerfil.equals(other.idPerfil))) {
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
             return false;
         }
         return true;
     }
 
-    @Override
-    public String toString() {
-        return "br.com.crescer.entity.Perfil[ idPerfil=" + idPerfil + " ]";
+    @XmlTransient
+    public List<Relacionamento> getRelacionamentos() {
+        return relacionamentos;
+    }
+
+    public void setRelacionamentos(List<Relacionamento> relacionamentos) {
+        this.relacionamentos = relacionamentos;
     }
 
     @XmlTransient
-    public List<Relacionamento> getRelacionamentoList() {
-        return relacionamentoList;
+    public List<Solicitacao> getSolicitacoes() {
+        return solicitacoes;
     }
 
-    public void setRelacionamentoList(List<Relacionamento> relacionamentoList) {
-        this.relacionamentoList = relacionamentoList;
-    }
-
-    @XmlTransient
-    public List<Relacionamento> getRelacionamentoList1() {
-        return relacionamentoList1;
-    }
-
-    public void setRelacionamentoList1(List<Relacionamento> relacionamentoList1) {
-        this.relacionamentoList1 = relacionamentoList1;
+    public void setSolicitacoes(List<Solicitacao> solicitacoes) {
+        this.solicitacoes = solicitacoes;
     }
 
     @XmlTransient
-    public List<Solicitacao> getSolicitacaoList() {
-        return solicitacaoList;
+    public List<Publicacao> getPublicacoes() {
+        return publicacoes;
     }
 
-    public void setSolicitacaoList(List<Solicitacao> solicitacaoList) {
-        this.solicitacaoList = solicitacaoList;
-    }
-
-    @XmlTransient
-    public List<Solicitacao> getSolicitacaoList1() {
-        return solicitacaoList1;
-    }
-
-    public void setSolicitacaoList1(List<Solicitacao> solicitacaoList1) {
-        this.solicitacaoList1 = solicitacaoList1;
-    }
-
-    @XmlTransient
-    public List<Publicacao> getPublicacaoList() {
-        return publicacaoList;
-    }
-
-    public void setPublicacaoList(List<Publicacao> publicacaoList) {
-        this.publicacaoList = publicacaoList;
+    public void setPublicacoes(List<Publicacao> publicacoes) {
+        this.publicacoes = publicacoes;
     }
 
 }
